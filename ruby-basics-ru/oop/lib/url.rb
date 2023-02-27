7fca70d6389f3ec2155537ba378b2df703f62678
@hexlet/cli <support@hexlet.io>
2023-02-27 19:42:58 +0300

    submit ruby-basics-ru/oop

--- ruby-basics-ru/oop/lib/url.rb
+++ ruby-basics-ru/oop/lib/url.rb
@@ -5,7 +5,7 @@ require 'uri'
 # BEGIN
 class Url
   extend Forwardable
-
+  
   attr_reader :address
   
   def initialize(url)
@@ -21,22 +21,28 @@ class Url
     @params.fetch(key, value)
   end
 
-  def <=>(url2)
-    return true if url2.scheme == url.scheme && url2.host == url.host && url2.port == url.port && url2.params.sort == url.params.sort
-    false 
-  end
-
-  def_delegators @address, :scheme, :host, :port
-
-  private
-  def set_params(address)
-    params = address.to_s.split('?').last.split('&')
-    params.each_with_object({}) do |param, acc|
-      args = param.split('=')
-      acc[args.first] ||= nil
-      acc[args.first] = args.last
+  def ==(url2)
+    if url2.scheme == self.scheme && 
+       url2.host == self.host && 
+       url2.port == self.port && 
+       url2.query_params.sort == self.query_params.sort
+    return true
+    else
+      return false
     end
   end
 
+  def_delegators :address, :scheme, :host, :port
+
+  private
+  def set_params(address)
+    return {} if !address.to_s.include? '?'
+    params = address.to_s.split('?').last.split('&')
+    params.each_with_object({}) do |param, acc|
+      args = param.split('=')
+      acc[args.first.to_sym] ||= nil
+      acc[args.first.to_sym] = args.last
+    end
+  end
 end
 # END
